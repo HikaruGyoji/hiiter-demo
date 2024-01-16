ce0be7ca728683cd1758fa2201b2f0baee324044
hikarugyoji <hikarugyoji@gmail.com>
2024-01-16 22:31:20 +0900

    change login css

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,36 +1,39 @@
 import { Link } from 'react-router-dom';
-import './styles/Login.css';
+import styles from './styles/Login.module.scss';
 import Header from './Header';
 
 function Login() {
   return (
-    <div className='Login'>
+    <div className={styles['Login']}>
       <Header name='ログイン' />
-      <div className='margin-area'>
-        <header className='Login-header'>
+      <div className={styles['margin-area']}>
+        <header className={styles['header']}>
           <input
-            className='input-box'
+            className={styles['input-box']}
             type='email'
             id='email'
             placeholder='メールアドレス'
           />
           <input
-            className='input-box'
+            className={styles['input-box']}
             type='password'
             id='password'
             placeholder='パスワード'
           />
 
-          <Link to='/signup' className='button -primary'>
+          <Link
+            to='/signup'
+            className={`${styles.button} ${styles['-primary']}`}
+          >
             メールアドレスでログイン
           </Link>
-          <Link to='/login' className='button -google'>
+          <Link to='/login' className={`${styles.button} ${styles['-google']}`}>
             Googleでログイン
           </Link>
-          <Link to='/signup' className='button -line'>
+          <Link to='/signup' className={`${styles.button} ${styles['-line']}`}>
             Lineでログイン
           </Link>
-          <Link to='/login' className='button -x'>
+          <Link to='/login' className={`${styles.button} ${styles['-x']}`}>
             Xでログイン
           </Link>
         </header>
