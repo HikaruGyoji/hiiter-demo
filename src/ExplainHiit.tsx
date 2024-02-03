fb202362f36b598ed6c6f2b38c82334f357c12c7
hikarugyoji <hikarugyoji@gmail.com>
2024-02-04 08:04:19 +0900

    add icon img

--- src/ExplainHiit.tsx
+++ src/ExplainHiit.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import Header from './Header';
-import React, { useState } from 'react';
 import styles from './styles/ExplainHiit.module.scss';
 
 function ExplainHiit() {
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,7 @@
 import styles from './styles/Header.module.scss';
 import { Link } from 'react-router-dom';
+import icon1 from './assets/img/icon1.png';
+import icon2 from './assets/img/icon2.png';
 
 function Header(props: { name: string; backPath: string; icons: boolean }) {
   return (
@@ -8,7 +10,14 @@ function Header(props: { name: string; backPath: string; icons: boolean }) {
         <div className={styles['header-wrapper']}>
           <Link to={props.backPath}>＜ 戻る</Link>
           <span>{props.name}</span>
-          {props.icons ? <div>icons</div> : <div>　　　</div>}
+          {props.icons ? (
+            <div>
+              <img src={icon1} alt='icon1' className={styles['icon-margin']} />
+              <img src={icon2} alt='icon2' />
+            </div>
+          ) : (
+            <div>　　　</div>
+          )}
         </div>
       </header>
     </div>
