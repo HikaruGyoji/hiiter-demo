3b3f7546913ed5cb24b7ae8876ecb10c5764c96a
hikarugyoji <hikarugyoji@gmail.com>
2024-02-04 08:22:20 +0900

    add sample img

--- src/ExplainHiit.tsx
+++ src/ExplainHiit.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import Header from './Header';
 import styles from './styles/ExplainHiit.module.scss';
+import muscle from './assets/img/muscle.png';
 
 function ExplainHiit() {
   return (
@@ -10,24 +11,33 @@ function ExplainHiit() {
         <p className={styles['userinfo-text']}>Hiitとは</p>
         <div className={styles['explain-box']}>
           <p>
+            <span className={styles['title']}>Hiitとは？</span>
+            <br />
             HIIT（High Intensity Interval
             Training）は、短い時間の高強度な運動と、低強度運動または休息を交互に行うトレーニング方法です。
             <br />
-            （図）
+            <div className={styles['img-box']}>
+              <img src={muscle} alt='img' />
+            </div>
             <br />
-            HIITは何が良いの？
+            <span className={styles['title']}>HIITは何が良いの？</span>
+            <br />
             HIITは時間効率が良く、脂肪燃焼、心肺機能の向上、筋肉の増強などに有効です。
             特に、タバタトレーニングはわずか4分間のトレーニングで1時間のランニングに匹敵する体力の向上（最大酸素摂取量）効果が期待できます。
             忙しい現代人にとって短時間で高い効果が得られる点が魅力です。
             さらに、中等度の強度で長く続ける長距離ランニングような「有酸素性」運動の能力と、大きなパワーを出すが長く続けられない100ｍ走のような「無酸素性」運動の能力に対して同時に最大の効果を得ることができます。
-            研究結果もいれる
             <br />
-            （図）
+            <div className={styles['img-box']}>
+              <img src={muscle} alt='img' />
+            </div>
             <br />
-            HIITの注意点は？
+            <span className={styles['title']}>HIITの注意点は？</span>
+            <br />
             過度のHIITは過労や怪我のリスクを増加させる可能性があります。初心者や特定の健康状態にある人は、医師やフィットネスの専門家と相談しながら慎重に進める必要があります。
             <br />
-            （図）
+            <div className={styles['img-box']}>
+              <img src={muscle} alt='img' />
+            </div>
           </p>
         </div>
         <header className={styles['userinfo-header']}>
