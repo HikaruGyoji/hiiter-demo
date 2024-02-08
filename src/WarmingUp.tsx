de7e42a9682584cac2734da3f71254bce5d555dd
hikarugyoji <hikarugyoji@gmail.com>
2024-02-08 15:31:02 +0900

    change style

--- src/WarmingUp.tsx
+++ src/WarmingUp.tsx
@@ -5,6 +5,9 @@ import VideoPlayer from './VideoPlayer';
 import trainingMenuData from './menu/trainingMenu.json';
 import testVideo from './assets/video/test.mp4';
 
+console.log(trainingMenuData.src);
+console.log(testVideo);
+
 function WarmingUp() {
   return (
     <div className={styles['userinfo1']}>
