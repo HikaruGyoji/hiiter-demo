b5bfc3103275e68fa41bf2ab474160e310134601
hikarugyoji <hikarugyoji@gmail.com>
2024-02-11 06:02:23 +0900

    add mui

---
+++ src/AccordionExpandDefault.tsx
@@ -0,0 +1,148 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function AccordionExpandDefault() {
+  return (
+    <div style={{ padding: '20px 30px', height: '1000px' }}>
+      <Accordion style={{ marginBottom: '4px' }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1-content'
+          id='panel1-header'
+        >
+          <Typography>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  textAlign: 'left',
+                  display: 'block',
+                }}
+              >
+                2024/02/11
+              </span>
+              <p style={{ margin: '0' }}>アップデートのお知らせ</p>
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography style={{ textAlign: 'left' }}>
+            デモ開発用のアップデートを行いました。
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion style={{ marginBottom: '4px' }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1-content'
+          id='panel1-header'
+        >
+          <Typography>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  textAlign: 'left',
+                  display: 'block',
+                }}
+              >
+                2024/02/11
+              </span>
+              <p style={{ margin: '0' }}>アップデートのお知らせ</p>
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography style={{ textAlign: 'left' }}>
+            デモ開発用のアップデートを行いました。
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion style={{ marginBottom: '4px' }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1-content'
+          id='panel1-header'
+        >
+          <Typography>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  textAlign: 'left',
+                  display: 'block',
+                }}
+              >
+                2024/02/11
+              </span>
+              <p style={{ margin: '0' }}>アップデートのお知らせ</p>
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography style={{ textAlign: 'left' }}>
+            デモ開発用のアップデートを行いました。
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion style={{ marginBottom: '4px' }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1-content'
+          id='panel1-header'
+        >
+          <Typography>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  textAlign: 'left',
+                  display: 'block',
+                }}
+              >
+                2024/02/11
+              </span>
+              <p style={{ margin: '0' }}>アップデートのお知らせ</p>
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography style={{ textAlign: 'left' }}>
+            デモ開発用のアップデートを行いました。
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion style={{ marginBottom: '4px' }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1-content'
+          id='panel1-header'
+        >
+          <Typography>
+            <div>
+              <span
+                style={{
+                  fontWeight: 'bold',
+                  textAlign: 'left',
+                  display: 'block',
+                }}
+              >
+                2024/02/11
+              </span>
+              <p style={{ margin: '0' }}>アップデートのお知らせ</p>
+            </div>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography style={{ textAlign: 'left' }}>
+            デモ開発用のアップデートを行いました。
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- src/Information.tsx
+++ src/Information.tsx
@@ -2,8 +2,7 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import styles from './styles/Information.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import AccordionExpandDefault from './AccordionExpandDefault';
 
 function Information() {
   return (
@@ -11,68 +10,7 @@ function Information() {
       <Header name='お知らせ' backPath='/home' icons={true} />
       <div className={styles['margin-area']}>
         <header className={styles['userinfo-header']}>
-          <ul className={styles['info-ul']}>
-            <li>
-              <div>
-                <span>2024/02/11</span>
-                <p>アップデートのお知らせ</p>
-              </div>
-              <FontAwesomeIcon
-                icon={faAngleRight}
-                className={styles['control-button']}
-              />
-            </li>
-            <li>
-              <div>
-                <span>2024/02/11</span>
-                <p>アップデートのお知らせ</p>
-              </div>
-              <FontAwesomeIcon
-                icon={faAngleRight}
-                className={styles['control-button']}
-              />
-            </li>
-            <li>
-              <div>
-                <span>2024/02/11</span>
-                <p>アップデートのお知らせ</p>
-              </div>
-              <FontAwesomeIcon
-                icon={faAngleRight}
-                className={styles['control-button']}
-              />
-            </li>
-            <li>
-              <div>
-                <span>2024/02/11</span>
-                <p>アップデートのお知らせ</p>
-              </div>
-              <FontAwesomeIcon
-                icon={faAngleRight}
-                className={styles['control-button']}
-              />
-            </li>
-            <li>
-              <div>
-                <span>2024/02/11</span>
-                <p>アップデートのお知らせ</p>
-              </div>
-              <FontAwesomeIcon
-                icon={faAngleRight}
-                className={styles['control-button']}
-              />
-            </li>
-            <li>
-              <div>
-                <span>2024/02/11</span>
-                <p>アップデートのお知らせ</p>
-              </div>
-              <FontAwesomeIcon
-                icon={faAngleRight}
-                className={styles['control-button']}
-              />
-            </li>
-          </ul>
+          <AccordionExpandDefault />
         </header>
       </div>
       <Footer />
