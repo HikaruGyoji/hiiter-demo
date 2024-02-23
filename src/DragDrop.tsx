1c7da3c13c21830b58823ff181546f494fe634d6
hikarugyoji <hikarugyoji@gmail.com>
2024-02-24 03:33:40 +0900

    add dad

--- src/DragDrop.tsx
+++ src/DragDrop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import {
   DragDropContext,
   Draggable,
@@ -8,59 +8,41 @@ import {
 import styles from './styles/DragDrop.module.scss';
 
 const DragDrop = () => {
-  // ドラッグ＆ドロップするアイテムのリスト
   const initialTasks = [
-    {
-      id: 'todo0',
-      text: 'スクワット',
-    },
-    {
-      id: 'todo1',
-      text: 'プッシュアップ',
-    },
-    {
-      id: 'todo2',
-      text: 'シットアップ',
-    },
-    {
-      id: 'todo3',
-      text: 'バックエクステンション',
-    },
+    { id: 'todo0', text: 'スクワット' },
+    { id: 'todo1', text: 'プッシュアップ' },
+    { id: 'todo2', text: 'シットアップ' },
+    { id: 'todo3', text: 'バックエクステンション' },
   ];
 
-  // 右側のアイテムリスト
   const fixedItems = [
-    {
-      id: 'item0',
-      text: 'スクワット',
-    },
-    {
-      id: 'item1',
-      text: 'プッシュアップ',
-    },
-    {
-      id: 'item2',
-      text: 'シットアップ',
-    },
-    {
-      id: 'item3',
-      text: 'バックエクステンション',
-    },
+    { id: 'item0', text: 'スクワット' },
+    { id: 'item1', text: 'プッシュアップ' },
+    { id: 'item2', text: 'シットアップ' },
+    { id: 'item3', text: 'バックエクステンション' },
   ];
 
-  // tasks ステートとその更新関数 setTasks を定義する
   const [tasks, setTasks] = useState(initialTasks);
 
-  // ドラッグ＆ドロップ操作終了時のハンドラー
+  useLayoutEffect(() => {
+    const setDraggableHeight = () => {
+      const draggables = document.querySelectorAll('.todo');
+      draggables.forEach((draggable) => {
+        if (draggable instanceof HTMLElement) {
+          draggable.style.height = `${draggable.offsetHeight}px`;
+        }
+      });
+    };
+    setDraggableHeight();
+  }, [tasks]);
+
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
 
-    // ドロップ先が存在しない場合は何もしない
     if (!destination) {
       return;
     }
 
-    // 左から右へのドラッグ＆ドロップ
     if (source.droppableId === 'items' && destination.droppableId === 'tasks') {
       const task = initialTasks[source.index];
       setTasks((prevTasks) => {
@@ -70,12 +52,6 @@ const DragDrop = () => {
       });
     }
 
-    // 右から左へのドラッグ＆ドロップ（無効にする）
-    if (source.droppableId === 'tasks' && destination.droppableId === 'items') {
-      return;
-    }
-
-    // 左から左へのドラッグ＆ドロップ（アイテムの入れ替え）
     if (source.droppableId === 'tasks' && destination.droppableId === 'tasks') {
       const draggedTask = tasks[source.index];
       const newTasks = [...tasks];
@@ -86,72 +62,70 @@ const DragDrop = () => {
   };
 
   return (
-    <>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <div className={styles['dragdrop-wrapper']}>
-          <Droppable droppableId='tasks'>
-            {(provided) => (
-              <div
-                className={styles['todoList']}
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {tasks.map((task, index) => (
-                  <Draggable key={task.id} draggableId={task.id} index={index}>
-                    {(provided) => (
-                      <div
-                        className={styles['todo']}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className={styles['dragdrop-wrapper']}>
+        <Droppable droppableId='tasks'>
+          {(provided) => (
+            <div
+              className={styles['todoList']}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(provided) => (
+                    <div
+                      className={styles['todo']}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {task.text}
+                      <button
+                        className={styles['deleteButton']}
+                        onClick={() => {
+                          const newTasks = [...tasks];
+                          newTasks.splice(index, 1);
+                          setTasks(newTasks);
+                        }}
                       >
-                        {task.text}
-                        <button
-                          className={styles['deleteButton']}
-                          onClick={() => {
-                            const newTasks = [...tasks];
-                            newTasks.splice(index, 1);
-                            setTasks(newTasks);
-                          }}
-                        >
-                          X
-                        </button>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-          <Droppable droppableId='items'>
-            {(provided) => (
-              <div
-                className={styles['itemList']}
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {fixedItems.map((item, index) => (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
-                    {(provided) => (
-                      <div
-                        className={styles['item']}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                      >
-                        {item.text}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </div>
-      </DragDropContext>
-    </>
+                        X
+                      </button>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId='items'>
+          {(provided) => (
+            <div
+              className={styles['itemList']}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {fixedItems.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided) => (
+                    <div
+                      className={styles['item']}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      {item.text}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 };
 
